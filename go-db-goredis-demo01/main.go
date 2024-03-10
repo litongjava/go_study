d18d4f1d16a607d29611d88d60e0927e41317841
litongjava <litongjava@qq.com>
2024-03-09 15:33:01 -1000

    add Hget

--- go-db-goredis-demo01/main.go
+++ go-db-goredis-demo01/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"go-db-goredis-demo01/redis"
+  "context"
+  "fmt"
+  "github.com/go-redis/redis/v8"
+  appRedis "go-db-goredis-demo01/redis"
 )
 
 func main() {
-	redis.InitializeRedisClient()
+  err := appRedis.InitializeRedisClient()
+  if err != nil {
+    panic(err)
+  }
+
+  val2, err := appRedis.Rdb.Get(appRedis.Ctx, "key2").Result()
+  if err == redis.Nil {
+    fmt.Println("key2 does not exist")
+  } else if err != nil {
+    panic(err)
+  } else {
+    fmt.Println("key2", val2)
+  }
+
+  hGetAllDemo(appRedis.Rdb, appRedis.Ctx)
+}
+
+func hGetAllDemo(rdb *redis.Client, ctx context.Context) {
+  v := rdb.HGetAll(ctx, "user").Val()
+  fmt.Println(v)
+  v2 := rdb.HMGet(ctx, "user", "name", "age").Val()
+  fmt.Println(v2)
+  v3 := rdb.HGet(ctx, "user", "age")
+  fmt.Println(v3)
 }
--- go-db-goredis-demo01/redis/redis.go
+++ go-db-goredis-demo01/redis/redis.go
@@ -1,21 +1,52 @@
 package redis
 
 import (
-	"context"
-	"github.com/go-redis/redis/v8"
+  "context"
+  "github.com/go-redis/redis/v8"
+  "time"
 )
 
 var Ctx = context.Background()
 var Rdb *redis.Client
 
 func InitializeRedisClient() (err error) {
-	Rdb = redis.NewClient(&redis.Options{
-		Addr:     "localhost:6391",
-		Password: "",
-		DB:       0,
-		// 连接池大小
-		PoolSize: 100,
-	})
-	_, err = Rdb.Ping(Ctx).Result()
-	return
+  opt, err := redis.ParseURL("redis://root:@localhost:6379/0")
+  if err != nil {
+    panic(err)
+  }
+  Rdb = redis.NewClient(opt)
+  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+  defer cancel()
+  _, err = Rdb.Ping(ctx).Result()
+  return err
+}
+
+//func InitializeRedisClient() (err error) {
+//  Rdb = redis.NewClient(&redis.Options{
+//    Addr:     "localhost:6391",
+//    Password: "",
+//    DB:       0,
+//    // 连接池大小
+//    PoolSize: 100,
+//  })
+//  _, err = Rdb.Ping(Ctx).Result()
+//  return
+//}
+
+//func initializeRedisClient() error {
+//  Rdb = redis.NewFailoverClient(&redis.FailoverOptions{
+//    MasterName:    "master",
+//    SentinelAddrs: []string{"10.0.0.10:6379", "10.0.0.11:6379", "10.0.0.11:6379"},
+//  })
+//  _, err := Rdb.Ping(Ctx).Result()
+//  return err
+//}
+
+func initializeRedisClient() error {
+  //var client *redis.ClusterClient
+  client := redis.NewClusterClient(&redis.ClusterOptions{
+    Addrs: []string{":7000", ":7001", ":7002", ":7003", ":7004", ":7006"},
+  })
+  _, err := client.Ping(Ctx).Result()
+  return err
 }
